97e54ac4f25b3de224a043036a801f49ac2a8d10
Rajpoot Himanshu <87543803+thehimanshoo@users.noreply.github.com>
2022-08-18 02:29:30 +0530

    final commit

--- src/Components/Layouts/About.js
+++ src/Components/Layouts/About.js
@@ -3,8 +3,15 @@ import React from "react";
 const About = () => {
     return(
         <React.Fragment>
-            <p className="lead h3 fw-bold text-primary">About Component</p>
-        </React.Fragment>
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col">
+                    <p className="h3 fw-bold text-secondary">About React Routing</p>
+                    <p className="lead px-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at consectetur consequuntur debitis enim, eum explicabo inventore iure mollitia natus, nobis non omnis quasi repellat sapiente sunt ullam veniam voluptatum? Alias ex labore, laborum quisquam sint vel voluptates! Aspernatur delectus eos pariatur? Accusantium earum facilis fugiat numquam quasi reiciendis, repellendus sequi tempora ut voluptate! A assumenda minima officiis perspiciatis ratione?</p>
+                </div>
+            </div>
+        </div>
+    </React.Fragment>
     )
 };
 export default About;
